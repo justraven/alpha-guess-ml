9a311ce15506f7ecd196f06f69d569bed803ff01
shofuuu <hadoepan@gmail.com>
2022-06-07 22:46:35 +0700

    First data analysis

--- src/Analysis.py
+++ src/Analysis.py
@@ -1,42 +1,21 @@
 #!/usr/bin/python3
 
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import numpy as np
-import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def plotPerColumnDistribution(df, nGraphShown, nGraphPerRow):
-    nunique = df.nunique()
-    df = df[[col for col in df if nunique[col] > 1 and nunique[col] < 50]]
-    nRow, nCol = df.shape
-    columnNames = list(df)
-    nGraphRow = (nCol + nGraphPerRow - 1) / nGraphPerRow
-    plt.figure(num = None, figsize = (6 * nGraphPerRow, 8 * nGraphRow), dpi = 80, facecolor = 'w', edgecolor = 'k')
-    for i in range(min(nCol, nGraphShown)):
-        plt.subplot(nGraphRow, nGraphPerRow, i + 1)
-        columnDf = df.iloc[:, i]
-        if (not np.issubdtype(type(columnDf.iloc[0]), np.number)):
-            valueCounts = columnDf.value_counts()
-            valueCounts.plot.bar()
-        else:
-            columnDf.hist()
-        plt.ylabel('counts')
-        plt.xticks(rotation = 90)
-        plt.title(f'{columnNames[i]} (column {i})')
-    plt.tight_layout(pad = 1.0, w_pad = 1.0, h_pad = 1.0)
-    plt.show()
+# Read in the data
+df = pd.read_csv('../data/dataset/01_Merged_alpha.csv', delimiter=',')
+print('--HEAD--\n', df.head())
+print('\n--DESCRIBE--\n', df.describe())
 
-if __name__ == '__main__':
-    print(os.listdir("../data/dataset"))
+# check target class percentage
+data = df['Alpha'].value_counts()
+data.plot(kind='pie', autopct='%1.1f%%', colors=['#006699', '#ff0000'])
+plt.show()
 
-    csv_fl = pd.read_csv('../data/dataset/01_Merged_alpha.csv', delimiter=',', nrows=1000)
-    csv_fl.dataframeName = 'Merged_alpha'
-    num_rows, num_cols = csv_fl.shape
-    print('Total rows: ', num_rows, ' | Total columns: ', num_cols)
+# check histogram of target class
+df.hist(figsize=(10, 10), bins=20, color='#006699')
+plt.show()
 
-    print('Head file')
-    print(csv_fl.head(5))
-    
-    plotPerColumnDistribution(csv_fl, 10, 3)
+# check correlation between features
+print('\n--CORRELATION--\n', df.corr())
